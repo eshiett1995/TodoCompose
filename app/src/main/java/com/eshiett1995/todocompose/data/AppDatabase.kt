f3fd9a79f937129dfdd8f4800941e0432a60682a
eshiett1995@gmail.com <Iam2fresh>
2021-05-15 15:16:27 +0100

    work in progress

--- app/src/main/java/com/eshiett1995/todocompose/data/AppDatabase.kt
+++ app/src/main/java/com/eshiett1995/todocompose/data/AppDatabase.kt
@@ -1,13 +1,29 @@
 package com.eshiett1995.todocompose.data
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.eshiett1995.todocompose.data.dao.TaskDao
 import com.eshiett1995.todocompose.data.entity.Task
-
+import androidx.room.Room
 
 @Database(entities = [Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun taskDao() : TaskDao
+    companion object{
+        private var INSTANCE: AppDatabase? = null
+        fun getAppDatabase(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "task-database"
+                )
+                .allowMainThreadQueries()
+                .build()
+            }
+            return INSTANCE
+        }
+    }
 }
--- app/src/main/java/com/eshiett1995/todocompose/viewModels/AddTaskViewModel.kt
+++ app/src/main/java/com/eshiett1995/todocompose/viewModels/AddTaskViewModel.kt
@@ -1,13 +1,15 @@
 package com.eshiett1995.todocompose.viewModels
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AddTaskViewModel : ViewModel() {
+class AddTaskViewModel(application: Application) : AndroidViewModel(application) {
     var title : MutableLiveData<String> = MutableLiveData("")
     var detail : MutableLiveData<String> = MutableLiveData("")
 
     fun saveTask(){
-            Log.i("hello from here", "Logger")
+
     }
 }
