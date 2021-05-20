dfdf532f2739a9d88b717d7349c5125ea62a082f
eshiett1995@gmail.com <Iam2fresh>
2021-05-20 12:40:49 +0100

    work in progress

--- app/src/main/java/com/eshiett1995/todocompose/activities/MainActivity.kt
+++ app/src/main/java/com/eshiett1995/todocompose/activities/MainActivity.kt
@@ -2,9 +2,11 @@ package com.eshiett1995.todocompose.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -18,8 +20,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -68,9 +72,21 @@ class MainActivity : AppCompatActivity() {
                         )
                         Text("Thursday, February 6", color = Color.White)
                         Spacer(modifier = Modifier.height(30.dp))
-                        if(tasks.value != null){
-                            tasks.value!!.forEach { task ->
-                                TaskCard(task)
+                        if (tasks.value != null){
+                            if(tasks.value!!.isNotEmpty()){
+                                tasks.value!!.forEach { task ->
+                                    TaskCard(task)
+                                }
+                            }else{
+                                Box(
+                                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                                ) {
+                                    Image(
+                                        painter = painterResource(R.drawable.ic_safebox),
+                                        contentDescription = "this is the empty box",
+                                        modifier = Modifier.height(100.dp).width(100.dp).align(alignment = Alignment.Center)
+                                    )
+                                }
                             }
                         }
                     }
@@ -83,6 +99,7 @@ class MainActivity : AppCompatActivity() {
     fun TaskCard(task : Task){
       Card (
           backgroundColor = Color.White,
+          //modifier = Modifer
       ){
          Row (
              verticalAlignment = Alignment.CenterVertically,
@@ -93,11 +110,15 @@ class MainActivity : AppCompatActivity() {
              CustomCheckBox()
              Spacer(modifier = Modifier.width(10.dp))
              Column {
-                 Text(task.title)
-                 Row{
-                     Text("Tasks.")
-                     Text("Friday, Feb 7, 9:00AM")
-                 }
+                 Text(task.title,
+                     fontWeight = FontWeight.Black,
+                     fontSize = 18.sp,
+                     fontFamily = FontFamily(poppinsRegularFont))
+                 Text(
+                     task.detail,
+                     fontFamily = FontFamily(poppinsRegularFont),
+                     color = Color.Gray
+                 )
              }
          }
       }
